d40128806bb456c1de072176fc69aaf62446b76d
CodeNinja0615 <akhtarsameer743@gmail.com>
2025-06-20 01:48:45 +0530

    trying a fix

--- test/pageobjects/Shopping.page.js
+++ test/pageobjects/Shopping.page.js
@@ -7,7 +7,7 @@ class Shopping extends Common {
         this.$$cards = () => $$('.card.h-100');
         // this.$cardTitle = () => $('.card-title');
         // this.$cardFotter = () => $('.card-footer .btn');
-        this.$checkout = () => $('//a[contains(text(),"Checkout")]');
+        this.$checkout = () => $('a.nav-link.btn.btn-primary');
     }
     async addProductToCart(productsList) {
         const cards = await this.$$cards(); // get all cards first
@@ -21,8 +21,14 @@ class Shopping extends Common {
             }
         }
     }
-    async checkout(){
+    async checkout() {
         await this.combinedClick(this.$checkout());
+        // await this.$checkout().waitForStable();
+        // await this.$checkout().scrollIntoView();
+        // await browser.execute(() => {
+        //     const el = document.querySelector('a.nav-link.btn.btn-primary');
+        //     if (el) el.click();
+        // });
     }
 
 }
