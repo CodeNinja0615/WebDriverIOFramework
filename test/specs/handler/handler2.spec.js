f241b73e1225596ac2fd2b5ec9166bcac779066c
CodeNinja0615 <akhtarsameer743@gmail.com>
2025-07-08 10:30:14 +0530

    new handler

---
+++ test/specs/handler/handler2.spec.js
@@ -0,0 +1,13 @@
+import dataSet from '../../test-data/dataSet01.json' with { type: 'json' };
+import runE2ETest from '../flows/end2end-ecommerce-flow.js';
+
+const testList = [runE2ETest]; // Add more if needed
+
+describe('Sequential Test Execution Handler', () => {
+    for (let i = 0; i < testList.length; i++) {
+        const flow = testList[i];
+        it(`Running test flow ${i + 1}`, async () => {
+            await flow(dataSet, i);
+        });
+    }
+});
